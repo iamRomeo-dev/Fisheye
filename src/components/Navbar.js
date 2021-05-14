f37f2d949c3b8ae0a1c6e39874a1c3d78fdc8768
iamRomeo-dev <romeo.brilland@live.fr>
2021-05-14 17:49:24 +0200

    :lipstick: :construction: Design the Navbar but still in progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,46 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-    return (
-        <div>
-            aaa
-        </div>
-    )
-}
+
+    // useEffect(() => {
+    //     getUsers(search).then((users) => setUsers(users));
+    //   }, [search]);
+
+  return (
+    <header>
+      <Link to="" className="logo">
+        <img src="public/img/logo.svg" alt="Fisheye Home page" />
+      </Link>
+      <nav aria-label="Main navigation">
+        <ul className="Tags">
+          <li className="Tag">
+            <Link to="#" className="TagLink">#Portrait</Link>
+          </li>
+          <li className="Tag">
+            <Link to="#" className="TagLink">#Art</Link>
+          </li>
+          <li className="Tag">
+            <Link to="#" className="TagLink">#Fashion</Link>
+          </li>
+          <li className="Tag">
+            <Link to="#" className="TagLink">#Architecture</Link>
+          </li>
+          <li className="Tag">
+            <Link to="#" className="TagLink">#Travel</Link>
+          </li>
+          <li className="Tag">
+            <Link to="#" className="TagLink">#Sport</Link>
+          </li>
+          <li className="Tag">
+            <Link to="#" className="TagLink">#Animals</Link>
+          </li>
+          <li className="Tag">
+            <Link to="#" className="TagLink">#Events</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1 className="NosPhotographes">Nos photographes</h1>
+    </header>
+  );
+};
